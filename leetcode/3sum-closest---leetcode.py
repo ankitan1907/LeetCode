0a847d95eca04c82171b73f355fe703975d70be5
Ankita N <165762358+ankitan1907@users.noreply.github.com>
2025-08-28 16:38:55 +0530

    Add python code snippet from 3Sum Closest - LeetCode

--- leetcode/3sum-closest---leetcode.py
+++ leetcode/3sum-closest---leetcode.py
@@ -1,25 +1,16 @@
-class Solution(object):
-    def threeSumClosest(self, nums, target):
-        nums.sort()
-        closest_sum = float('inf')
-        
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-                
-            left, right = i + 1, len(nums) - 1
-            
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-                
-                if abs(current_sum - target) < abs(closest_sum - target):
-                    closest_sum = current_sum
-                
-                if current_sum < target:
-                    left += 1
-                elif current_sum > target:
-                    right -= 1
-                else:
-                    return current_sum
-        
-        return closest_sum
+
+
+class Solution {
+
+    public int peakElement(int[] arr) {
+        int n=arr.length;
+        int peak=0;
+        for(int i=1;i<n;i++){
+            if(arr[i-1]<arr[i])
+                peak=i;
+        }
+        return peak;
+        }
+       
+    }
+
