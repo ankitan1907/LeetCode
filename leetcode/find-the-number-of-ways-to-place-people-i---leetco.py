300966c17fabfb8e8a41ab0e855ab3194d260ff5
Ankita N <165762358+ankitan1907@users.noreply.github.com>
2025-09-03 21:42:14 +0530

    Add python code snippet from Find the Number of Ways to Place People I - LeetCode

--- leetcode/find-the-number-of-ways-to-place-people-i---leetco.py
+++ leetcode/find-the-number-of-ways-to-place-people-i---leetco.py
@@ -1,30 +1,30 @@
-public class Solution {
+class Solution {
     public int numberOfPairs(int[][] points) {
-        int n = points.length;
+        int count=0;
+        int n=points.length;
 
-    
-        Arrays.sort(points, (a, b) -> (a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]));
+        for(int i=0; i<n; i++) {
+            int x1=points[i][0],y1=points[i][1];
+            for(int j=0; j<n; j++) {
+                if(i==j) continue;
+                int x2=points[j][0],y2=points[j][1];
+                if(x1>x2 || y1<y2) continue;
 
-        int count = 0;
+                boolean flag=true;
+                for(int k=0; k<n; k++) {
+                    if(k==i || k==j) continue;
+                    int x=points[k][0],y=points[k][1];
 
-        // For each left point (p1)
-        for (int i = 0; i < n; i++) {
-            TreeSet<Integer> ySeen = new TreeSet<>();
-            int[] p1 = points[i];
-
-            for (int j = i + 1; j < n; j++) {
-                int[] p2 = points[j];
-                if (p1[0] <= p2[0] && p1[1] >= p2[1]) {
-                    // Find if any y in ySeen lies between [p2.y, p1.y]
-                    Integer yInRange = ySeen.ceiling(p2[1]); // smallest >= p2.y
-                    if (yInRange == null || yInRange > p1[1]) {
-                        count++; // valid pair
-                    }
+                    if(x>=x1 && x<=x2 && y<=y1 && y>=y2) {
+                        flag=false;
+                        break;
+                    }                        
                 }
-                // Insert p2.y for future checks
-                ySeen.add(p2[1]);
+                if(flag==true) count++;
             }
         }
+
         return count;
     }
+
 }
